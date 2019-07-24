761ee149519ce53efdff40618a0862cdc6b57d8b
Ghabi Lassad <ghabiassaad@gmail.com>
2019-07-25 00:05:03 +0100

    Architecture de projet gérrant les équipements de l'entreprise

--- CompanyEquip/App_Start/RouteConfig.cs
+++ CompanyEquip/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CompanyEquip
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Equipments", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
